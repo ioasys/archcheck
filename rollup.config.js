0c9eab1a2cf63bb15c9181905f313bc3e2f9ed51
Nelson Sinis <nelsonsinis@ioasys.com.br>
2024-10-01 16:33:56 -0300

    feat: initial commit

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.mjs',
+      format: 'es',
+    },
+  ],
+  plugins: [typescript()],
+  external: [],
+};
